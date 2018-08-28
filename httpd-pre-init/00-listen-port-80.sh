1ecbd57e996476b288730a84d63210c00414cba0
R Valk <rvalk@deloitte.com.au>
2018-08-28 16:42:17 +1000

    listen on 8090

--- httpd-pre-init/00-listen-port-80.sh
+++ httpd-pre-init/00-listen-port-80.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-echo "Changing HTTP listener port from 8080 to 80..."
-sed -i -e 's/8080/80/' /etc/httpd/conf/httpd.conf
+echo "Changing HTTP listener port from 8080 to 8090..."
+sed -i -e 's/8080/8090/' /etc/httpd/conf/httpd.conf
 echo "...done"
